03bddfba6e0e664110d795a82f8f8ff97676bf44
black15 <ousstalha77@gmail.com>
2023-02-02 11:35:07 +0100

    Navbar

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import { Poppins } from '@next/font/google'
 import Link from 'next/link'
-import { useTheme } from 'next-themes'
 import SwitchTheme from '../ui/Switch'
 
 type Props = {}
@@ -9,26 +9,23 @@ const poppins = Poppins({ weight: ["300", "400", "500", "600", "700"] })
 
 const Header = (props: Props) => {
 	
-	const {systemTheme , theme, setTheme} = useTheme();
-
 	return (
-		<header className={`flex flex-row items-center justify-around px-6 py-2 ${poppins.className}`}>
-		<div>
-			<h1 className={`text-3xl text-gray-100 font-semibold`}>O-Dev</h1>
-		</div>
-		<div>
-			<ul className="flex items-center space-x-4">
-				<li><Link href={""}>About Me</Link></li>
-				<li><Link href={""}>Skills</Link></li>
-				<li><Link href={""}>Projects</Link></li>
-			</ul>
-		</div>
-		<div className='flex items-center space-x-2'>
-			<Link href={''}></Link>
-		</div>
-		<div>
-			<SwitchTheme />
-		</div>
+		<header className={`flex flex-row items-center justify-evenly px-6 py-2 bg-transparent w-screen${poppins.className}`}>
+			<div className='w-1/3'>
+				<h1 className={`text-3xl font-semibold`}>O-Dev</h1>
+			</div>
+			<div className='w-1/3'>
+				<ul className="flex items-center space-x-4 font-medium uppercase">
+					<li className=' transition hover:text-purple-600'><Link href={""}>About Me</Link></li>
+					<li className=' transition hover:text-purple-600'><Link href={""}>Skills</Link></li>
+					<li className=' transition hover:text-purple-600'><Link href={""}>Projects</Link></li>
+				</ul>
+			</div>
+			
+			<div className='flex items-center space-x-2'>
+				<button className='px-6 py-2 border-2 border-gray-900 dark:border-gray-200 transition hover:bg-purple-700 hover:animate-wiggle '>Let's GO</button>
+				<SwitchTheme />
+			</div>
 		</header>
   )
 }
--- components/ui/Switch.tsx
+++ components/ui/Switch.tsx
@@ -14,26 +14,28 @@ export default function SwitchTheme() {
 			
   }
   return (
-	<Switch
-		checked={enabled}
-		onChange={() => renderThemeChanger()}
-		className={`${enabled ? 'bg-gray-500' : 'bg-gray-700'}
-			relative inline-flex h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-	>
-		<span className="sr-only">Use setting</span>
-		<span
-		 aria-hidden="true"
-		 className={`${enabled ? 'translate-x-6' : 'translate-x-0'}
-		 flex items-center pointer-events-none h-[20px] w-[20px] transform rounded-full shadow-lg ring-0 transition duration-200 ease-in-out`}>
-			{enabled 
-				? 
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width={29} className="text-yellow-500" role="button">
-					<path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
-				</svg>
-				: 
-				<svg role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width={27} className="text-gray-100"> <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clipRule="evenodd" /> </svg>
-			}
-		</span>
-	</Switch>
+	<div className='transform rotate-90'>
+		<Switch
+			checked={enabled}
+			onChange={() => renderThemeChanger()}
+			className={`${enabled ? 'bg-gray-700' : 'bg-gray-300'}
+				relative inline-flex h-6 w-12 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+		>
+			<span className="sr-only">Use setting</span>
+			<span
+			aria-hidden="true"
+			className={`${enabled ? 'translate-x-0' : 'translate-x-6'}
+			flex items-center pointer-events-none h-[20px] w-[20px] transform rounded-full shadow-lg ring-0 transition duration-200 ease-in-out`}>
+				{enabled 
+					? 
+					<svg role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width={27} className=" transform rotate-180 text-gray-100"> <path fillRule="evenodd" d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z" clipRule="evenodd" /> </svg>
+					: 
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width={29} className="text-yellow-500" role="button">
+						<path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+					</svg>
+				}
+			</span>
+		</Switch>
+	</div>
   )
 }
