eaf7408baad246e2503c1e753584bcaef17a4272
black15 <ousstalha77@gmail.com>
2023-02-05 17:35:30 +0100

    Margin for skills section

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -9,7 +9,7 @@ const Skills = (props: Props) => {
     <section className='relativemb-32 lg:mb-12'>
       <h1 className="text-center absolute right-0 left-0 mx-auto text-5xl sm:text-7xl lg:text-9xl text-[#0000002c] dark:text-[#ffffff0e] font-bold animate__animated animate__fadeInUp -z-10">SKILLS</h1>
 			<h2 className="text-2xl md:text-3xl text-center font-bold z-10 uppercase animate__animated animate__fadeInUp pt-6 lg:pt-10">Tec<span className='border-b-4 border-[#009945]'>hnologie</span>s ?</h2>
-      <div className="flex flex-row flex-wrap justify-between items-center mt-24">
+      <div className="flex flex-row flex-wrap justify-between items-center my-24">
         <Skill name='HTML' width='90%' />
         <Skill name='CSS/Tailwind' width='80%' />
         <Skill name='Javascript' width='80%' />
